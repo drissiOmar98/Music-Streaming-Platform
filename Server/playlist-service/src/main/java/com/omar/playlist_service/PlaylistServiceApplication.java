e686b3176b1d9dc7e42147035dde8300b1c7b3dc
DrissiOmar <omar.drissi@esprit.tn>
2025-03-15 23:58:42 +0100

    feat: implement security configuration with Keycloak , auditing, Feign client support, and OpenAPI documentation

--- Server/playlist-service/src/main/java/com/omar/playlist_service/PlaylistServiceApplication.java
+++ Server/playlist-service/src/main/java/com/omar/playlist_service/PlaylistServiceApplication.java
@@ -2,8 +2,12 @@ package com.omar.playlist_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableFeignClients
 public class PlaylistServiceApplication {
 
 	public static void main(String[] args) {
