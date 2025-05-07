906cc5c354de9dd16409a53eb38af253bb6dd904
DrissiOmar <omar.drissi@esprit.tn>
2025-05-07 23:24:04 +0100

    initialize event-participation-service for managing user interactions with events

---
+++ Server/event-participation-service/src/test/java/com/omar/event_participation_service/EventParticipationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.event_participation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventParticipationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
